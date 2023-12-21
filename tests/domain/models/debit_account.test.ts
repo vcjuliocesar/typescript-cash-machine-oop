2c545f7876a1d53eee40fb6bb6fa2fe0cc2489f4
vcjuliocesar <vcjuliocesar@outlook.com>
2023-12-21 14:19:42 -0600

    test:add account test

---
+++ tests/domain/models/debit_account.test.ts
@@ -0,0 +1,90 @@
+import { Debit } from "../../../src/domain/models/debit"
+import { UserEntity } from "../../../src/domain/models/user"
+import { INITIALIZE_BALANCE_AMOUNT,MAX_AMOUNT_AVAILABLE,MIN_AMOUNT_AVAILABLE} from "../../../src/domain/value_objects"
+
+describe('Debit account',()=>{
+    
+    it('returns an exception if amount is zero',()=>{
+        
+        const name = 'Jhon Doe'
+        
+        const email = 'jhon.doe@yopmail.com'
+
+        const amount = 0
+
+        const user = new UserEntity(name,email)
+
+        user.register()
+
+        const account = new Debit()
+
+        expect(()=>{
+            account.with_drawal(amount)
+        }).toThrow(`Indica una cantidad mayor a ${amount}`)
+    })
+
+    it('returns an exception if amount is greater than balance',()=>{
+        
+        const name = 'Jhon Doe'
+        
+        const email = 'jhon.doe@yopmail.com'
+
+        const amount = 10000000
+
+        const user = new UserEntity(name,email)
+
+        user.register()
+
+        const account = new Debit()
+
+        expect(()=>{
+            account.with_drawal(amount)
+        }).toThrow(`La cantidad ${amount} es mayor al saldo disponible`)
+    })
+
+    it('returns debit amount balance',()=>{
+        
+        const amount = 100
+
+        const account = new Debit()
+
+        account.with_drawal(amount)
+
+        expect(account.balance()).toBe(INITIALIZE_BALANCE_AMOUNT - amount)
+    })
+
+    it('saving debit amount balance',()=>{
+        
+        const amount = 100
+
+        const account = new Debit()
+
+        account.saving(amount)
+
+        expect(account.balance()).toBe(INITIALIZE_BALANCE_AMOUNT + amount)
+    })
+
+    it('returns an exception if amount is greater than MAX_AMOUNT_AVAILABLE',()=>{
+
+        const amount = 100000000
+
+        const account = new Debit()
+
+        expect(()=>{
+            account.saving(amount)
+        }).toThrow(`No se permiten transacciones superiores o iguales a ${MAX_AMOUNT_AVAILABLE}`)
+    })
+
+    it('returns an exception if amount is less than MIN_AMOUNT_AVAILABLE',()=>{
+
+        const amount = 1
+
+        const account = new Debit()
+
+        expect(()=>{
+            account.saving(amount)
+        }).toThrow(`No se permiten transacciones menores a ${MIN_AMOUNT_AVAILABLE}`)
+    })
+
+
+})
