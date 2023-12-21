3f2c0ef0f93d230aaeffc9fd81ad846af16860e5
vcjuliocesar <vcjuliocesar@outlook.com>
2023-12-21 15:19:16 -0600

    feat: add new  constants

--- src/domain/value_objects.ts
+++ src/domain/value_objects.ts
@@ -1,3 +1,6 @@
 export const INITIALIZE_BALANCE_AMOUNT:number = 1100
 export const MAX_AMOUNT_AVAILABLE:number = 100000000
 export const MIN_AMOUNT_AVAILABLE:number = 10
+export const INITIALIZE_CREDIT_BALANCE_AMOUNT:number = 500
+export const FEE:number = 0.05
+export const LIMIT_CREDIT:number = 400000
